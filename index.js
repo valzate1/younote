d4fd8be12d30a0d5cd5eb27c5b92110a10f1d2dd
valen <valzate1@jhu.edu>
2020-11-05 14:23:16 -0500

    added heroku

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 const app = express();
 const port = process.env.PORT || 7000;
+const axios = require("axios");
+const API_URL = "https://younote-api-db.herokuapp.com/";
+
 
 nunjucks.configure("views", {
   express: app,
@@ -17,8 +20,16 @@ app.get("/dashboard", async (req, res) => {
   const username = req.query.uname;
   const data = {
     nickname: username,
-    notes: []
   };
+
+  try {
+    let response = await axios.get(`${API_URL}/api/notes?author=${username}`);
+    data.notes = response.data.data;
+  } catch (error) {
+    console.log(error);
+    data.notes = [];
+  }
+
   res.render("dashboard.njk", data);
 });
 
